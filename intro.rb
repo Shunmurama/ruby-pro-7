ea0bbcaccfdd28219224370aeed41a23e292bcc5
Shunmurama <mura.faily72@gmail.com>
2024-01-13 17:35:37 +0000

    ~p237

--- intro.rb
+++ intro.rb
@@ -1,11 +1,32 @@
-users = []
-users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
-users << { first_name: 'Bob', last_name: 'Python', age: 30 }
+class User
+  attr_reader :first_name, :last_name, :age
 
-def full_name(user)
-  "#{user[:first_name]} #{user[:last_name]}"
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
+
+
+
+users = []
+users << User.new('Alice', 'Ruby', 20)
+users << User.new('Bob', 'Python', 30)
+
 
 users.each do |user|
-  puts "氏名: #{full_name(user)}, 年齢: #{user[:age]}"
+  puts "氏名: #{user.full_name}, 年齢: #{user.age}"
 end
+
+puts users[1].last_name
+
+# classを作ると属性を変更できない
+# users[0].country = 'japan'
+# users[0].first_name = 'Carol'
+puts users[0].first_name
