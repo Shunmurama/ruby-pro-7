3ff64faf4bc029c04ef0419f2744279b6cd02350
Shunmurama <mura.faily72@gmail.com>
2024-01-13 17:15:39 +0000

    ~p235

---
+++ intro.rb
@@ -0,0 +1,7 @@
+users = []
+users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
+users << { first_name: 'Bob', last_name: 'Python', age: 30 }
+
+users.each do |user|
+  puts "氏名: #{user[:first_name]} #{user[:last_name]}, 年齢: #{user[:age]}"
+end
